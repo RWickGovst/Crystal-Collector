70802927f7da51ca0aab5aa49ea4544be2e57971
Triumph <archie200@icloud.com>
2019-06-05 17:42:59 -0500

    spelling error

--- assets/main.js
+++ assets/main.js
@@ -22,7 +22,7 @@ var goldValue=Math.floor(Math.random() * 21) +16;
 // click a gem button
 // $("#win").text(wins);
 // for(var i=0;i<4;i++){
-$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 AT 8.13.39 AM.png' height='125px'>");
+$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 8.13.39 AM.png' height='125px'>");
 $("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.40.16 PM.png' height='125px'>");
 $("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.40.49 PM.png' height='125px'>");
 $("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.41.10 PM.png' height='125px'>");
