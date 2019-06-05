11fbd1305827ea22207222ab49c1dbbed5d5b31a
Triumph <archie200@icloud.com>
2019-06-05 16:55:44 -0500

    set up game board

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
+var wins=10;
+var losses;
+var totalScore;
+var computerNumber;
 var score;
-var crystalValue;
-var counter;
-
-
-$(".crystal-image").on("click", function() {
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    
-    counter += crystalValue;
-})
+// $("#win").text(wins);
+// for(var i=0;i<4;i++){
+$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 AT 8.13.39 AM.png' height='125px'>");
+$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.40.16 PM.png' height='125px'>");
+$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.40.49 PM.png' height='125px'>");
+$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.41.10 PM.png' height='125px'>");
+// }
