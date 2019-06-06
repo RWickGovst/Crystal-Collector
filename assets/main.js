ab532e9e16435c810c4b950d3f5d020231ca929b
Triumph <archie200@icloud.com>
2019-06-05 20:57:54 -0500

    small image tweak

--- assets/main.js
+++ assets/main.js
@@ -1,48 +1,74 @@
-var wins;
-var losses;
-var totalScore;
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
 var computerNum;
 var score;
-// select and display the computer pick
-var computerNum = Math.floor(Math.random() * 150) +50;
-// console.log(computerNum);
-$("#number-to-guess").html(computerNum);
 
+function randomNumCrystal() {
+    return {
+        blue: {
+            points: Math.floor(Math.random() * 5) + 1,
+            imageUrl: "assets/pictures/blueGem.png",
+        },
+        purple: {
+            points: Math.floor(Math.random() * 11) + 6,
+            imageUrl: "assets/pictures/purpleGem.png",
+        },
+        orange: {
+            points: Math.floor(Math.random() * 16) + 11,
+            imageUrl: "assets/pictures/orangeGem.png",
+        },
+        gold: {
+            points: Math.floor(Math.random() * 21) + 16,
+            imageUrl: "assets/pictures/goldGem.png",
+        }
+    }
+}
+// display computer number
+function startGame() {
+    totalScore = 0;
+    crystals = randomNumCrystal();
+    // select and display the computer pick
+    computerNum = Math.floor(Math.random() * 150) + 50;
+    // console.log(computerNum);
+    $("#number-to-guess").html(computerNum);
+}
 // assign a random value to each gem
-var blueValue= Math.floor(Math.random() * 5) +1;
-// console.log(blueValue);
-var purpleValue= Math.floor(Math.random() * 11) +6;
-// console.log(purpleValue);
-var orangeValue=Math.floor(Math.random() * 16) +11;
-// console.log(orangeValue);
-var goldValue=Math.floor(Math.random() * 21) +16;
-// console.log(goldValue);
+// display each gem
+function renderCrystal() {
+    for (var key in crystals) {
+        var crystalDiv = $("<div class='crystal-button' data-name='" + key + "'>");
+        var crystalImage = $("<img alt='image' class='crystal-img'>").attr("src", crystals[key].imageUrl);
+        crystalDiv.append(crystalImage);
+        $("#crystal-image").append(crystalDiv);
+        console.log(crystalDiv)
+    }
+}
+
+
+startGame();
+renderCrystal();
 
 
 // click a gem button
-// $("#crystal-mage").on("click", function() {
-// console.log(event);
-$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 8.13.39 AM.png' height='125px'>");
-$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.40.16 PM.png' height='125px'>");
-$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.40.49 PM.png' height='125px'>");
-$("#crystal-image").append("<img src='assets/pictures/Screen Shot 2019-06-05 at 4.41.10 PM.png' height='125px'>");
-// }
-
+$(".crystal-button").on("click", function (event) {
+    // totalScore = totalScore + crystals[crystal.attr("data-name")].points;
+});
 // when the gem is clicked, its value is added to totalScore
 // totalScore = totalScore + (clicked gem value);
-//  $("#total-score").html(totalScore);
+$("#total-score").html(totalScore);
 // check if totalScore > < or = computerNum
 
 // if(totalScore === computerNum){
-// alert "you win"
+// alert ("You win");
 // wins++;
-// $("#win").html(wins);
+$("#win").html(wins);
 // }
 
 // else if(totalScore > computerNum){
-// alert you lose
+// alert ("You lose!");
 // losses++;
-// $("#loss").html(losses);
+$("#loss").html(losses);
 // }
 
 // else(totalScore < computerNum){
